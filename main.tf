64d1a876d021815ff70f7fb627e080a863d5265f
EC2 Default User <ec2-user@ip-172-31-18-186.ec2.internal>
2022-10-10 19:05:15 +0000

    Assign2: update 1

--- main.tf
+++ main.tf
@@ -91,3 +91,35 @@ resource "aws_route_table_association" "public_routes" {
   route_table_id = aws_route_table.public_subnets.id
   subnet_id      = aws_subnet.public_subnet[count.index].id
 }
+
+resource "aws_eip" "nat_gateway" {
+  vpc = true
+}
+
+
+# Create NAT gateway 
+resource "aws_nat_gateway" "nat_gateway" {
+  allocation_id = aws_eip.nat_gateway.id
+  subnet_id     = aws_subnet.public_subnet[0].id
+
+  tags = {
+    Name = "Public NAT gateway"
+  }
+  
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.igw.id]
+}
+
+resource "aws_route_table" "instance" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gateway.id
+  }
+}
+
+resource "aws_route_table_association" "instance" {
+  subnet_id = aws_subnet.instance.id
+  route_table_id = aws_subnet.public_subnet[0].id
+}
